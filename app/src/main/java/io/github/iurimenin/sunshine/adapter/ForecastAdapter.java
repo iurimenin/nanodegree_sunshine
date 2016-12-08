dc2b3fbf19a0fd56b0fe8841a871bf898ae97206
Iuri <iuri.menin@softfocus.com.br>
2016-12-08 17:45:03 -0200

    Leason 5  - New list layout

--- app/src/main/java/io/github/iurimenin/sunshine/adapter/ForecastAdapter.java
+++ app/src/main/java/io/github/iurimenin/sunshine/adapter/ForecastAdapter.java
@@ -7,7 +7,6 @@ import android.support.v4.widget.CursorAdapter;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -46,8 +45,8 @@ public class ForecastAdapter extends CursorAdapter {
         // our view is pretty simple here --- just a text view
         // we'll keep the UI functional with a simple (and slow!) binding.
 
-        TextView tv = (TextView)view;
-        tv.setText(convertCursorRowToUXFormat(cursor));
+        //TextView tv = (TextView)view;
+        //tv.setText(convertCursorRowToUXFormat(cursor));
     }
 
     /* The date/time conversion code is going to be moved outside the asynctask later,
