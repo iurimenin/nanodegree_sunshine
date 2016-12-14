26a8cdd121aed3d8b523f72497f27538d735ed5b
iurimenin <iuri020293@>
2016-12-14 17:23:04 -0200

    Lesson 5 - Tablet layout - Started

--- app/src/main/java/io/github/iurimenin/sunshine/activity/MainActivity.java
+++ app/src/main/java/io/github/iurimenin/sunshine/activity/MainActivity.java
@@ -9,27 +9,37 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 import io.github.iurimenin.sunshine.R;
+import io.github.iurimenin.sunshine.fragment.DetailFragment;
 import io.github.iurimenin.sunshine.fragment.ForecastFragment;
 import io.github.iurimenin.sunshine.utils.Utility;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String LOG_TAG = MainActivity.class.getSimpleName();
-    private static final String FORECASTFRAGMENT_TAG = "FFTAG";
+    private static final String DETAILFRAGMENT_TAG = "DFTAG";
 
     private String mLocation;
+    private boolean mTwoPane;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        mLocation = Utility.getPreferredLocation(this);
         super.onCreate(savedInstanceState);
+
+        mLocation = Utility.getPreferredLocation(this);
         setContentView(R.layout.activity_main);
-        if (savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, new ForecastFragment(), FORECASTFRAGMENT_TAG)
-                    .commit();
+        if (findViewById(R.id.weather_detail_container) != null) {
+            mTwoPane = true;
+
+            if (savedInstanceState == null) {
+                getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.weather_detail_container,
+                                new DetailFragment(), DETAILFRAGMENT_TAG)
+                        .commit();
+            }
+        } else {
+            mTwoPane = false;
         }
-        Log.d(LOG_TAG, "onCreate");
+
     }
 
     @Override
@@ -88,7 +98,7 @@ public class MainActivity extends AppCompatActivity {
         String location = Utility.getPreferredLocation( this );
         // update the location in our second pane using the fragment manager
         if (location != null && !location.equals(mLocation)) {
-            ForecastFragment ff = (ForecastFragment)getSupportFragmentManager().findFragmentByTag(FORECASTFRAGMENT_TAG);
+            ForecastFragment ff = (ForecastFragment)getSupportFragmentManager().findFragmentById(R.id.fragment_forecast);
             if ( null != ff ) {
                 ff.onLocationChanged();
             }
